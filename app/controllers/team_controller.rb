64dc85a07c98277d86551bd4664f7fcec5055248
alexlemia13 <alexlemia13@outlook.fr>
2020-10-26 15:16:39 +0100

    moitié

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,5 @@
+class TeamController < ApplicationController
+  def introduce
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'Welcome#'
+  get '/team', to:'team#introduce'
+  get '/contact_us', to:'contact#contact_us'
+  get '/welcome/:first_name', to:'welcome#first_name'
+
 end
